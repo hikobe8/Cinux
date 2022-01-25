bd68bbaf74cd802debdb730caba24b5fc21b2faf
yurui <taia@zhumanggroup.com>
2022-01-25 18:51:00 +0800

    first

---
+++ datalab-handout/why.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+int main(){
+    // int a = 1;
+    // if (a)
+    // {
+    //     printf("fuck a = %d\n", a);
+    // }
+    
+    // printf("%d\n", !!a);
+    // int b =  0x0fffffff;
+    // printf("%d\n", !b);
+    // int c = 0;
+    // printf("%d\n", !c);
+    printf("%d\n",  1 - (1<<32));
+    return 0;
+}
